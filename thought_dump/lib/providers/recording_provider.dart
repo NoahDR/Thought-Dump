c723afbc105f630420c8babfd8b5baa2e3bf32ae
Noah Reinbold <noahdaniel.reinbold@yahoo.com>
2024-09-27 14:42:38 +0200

    recording

--- thought_dump/lib/providers/recording_provider.dart
+++ thought_dump/lib/providers/recording_provider.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import '../utils/recording_logic.dart';
 
 class RecordingProvider with ChangeNotifier {
+  final RecordingLogic _recordingLogic = RecordingLogic();
   bool _isRecording = false;
 
   bool get isRecording => _isRecording;
 
-  void toggleRecording() {
+  Future<void> toggleRecording() async {
+    if (_isRecording) {
+      await _recordingLogic.stopListening();
+    } else {
+      await _recordingLogic.startListening((result) {});
+    }
     _isRecording = !_isRecording;
     notifyListeners();
+  }
+
+  Future<void> initialize() async {
+    await _recordingLogic.initialize();
   }
 }
